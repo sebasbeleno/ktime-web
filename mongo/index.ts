2051b23df14a3458420a3900639c995e2887db6d
Sebastian Beleño <sebasbeleno15@gmail.com>
2022-10-05 18:11:09 -0500

    fix: linter

--- mongo/index.ts
+++ mongo/index.ts
@@ -3,8 +3,6 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 const uri = process.env.MONGODB_URI as string;
 
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
